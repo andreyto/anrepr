c9becb56b7e7bab328896d2a233833b4ff90f5b1
Andrey Tovchigrechko <tovchigrechkoa@medimmune.com>
2016-11-25 18:56:52 -0500

    Vignette and improvements to API docs. This is an attempt to resolve friction between knitr vignette generation (assumes that output is a single HTML) and my need to include the entire sample report output (directory tree with files). My solution is to run the example from vignette .Rmd, have the code shown and the output files discarded. A link to the would-be output files is manually entered into the .Rmd, the example re-run externally to .Rmd and the report directory is copied into the inst/docs.

---
+++ R/anrepr.R
@@ -0,0 +1,13 @@
+#' Analysis Reporting in R
+#'
+#' Anrepr provides a report generator that acts as a graphical logger
+#' turning the output of your analysis pipeline into a static Web site
+#'
+#' The main entry point is the constructor of the \code{\link{anrep}}
+#' class.
+#'
+#' \strong{INSTALLATION NOTE:} This package needs the Pandoc Markdown
+#' converter executable installed on your system. You can refer to the documentation of the
+#' \href{https://CRAN.R-project.org/package=pander}{\pkg{pander}} package for
+#' the alternative ways of installing Pandoc.
+"_PACKAGE"
